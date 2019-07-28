1261670b7831d0d228a8e6b42b1bd5d21cff7ab1
Olexyy <olexyy.mails@gmail.com>
2019-07-28 11:04:32 +0300

    Refactor testing structure.

---
+++ web_service/tests/MultipleModelTestCase.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests;
+
+use RestfulWS\Core\Components\Factory\BookFactory;
+use RestfulWS\Core\Components\Factory\CategoryFactory;
+use RestfulWS\Core\Components\Factory\ModelFactoryInterface;
+use RestfulWS\Core\Components\Model\Book;
+use RestfulWS\Core\Components\Model\Category;
+use RestfulWS\Core\Components\Storage\StorageInterface;
+
+/**
+ * Class MultipleModelTestCase.
+ *
+ * @package Tests
+ */
+class MultipleModelTestCase extends KernelTestCase {
+
+  /**
+   * Generator.
+   *
+   * @var ModelFactoryInterface
+   */
+  protected $bookFactory;
+
+  /**
+   * Storage.
+   *
+   * @var StorageInterface
+   */
+  protected $bookStorage;
+
+  /**
+   * Generator
+   *
+   * @var ModelFactoryInterface
+   */
+  protected $categoryFactory;
+
+  /**
+   * Storage.
+   *
+   * @var StorageInterface
+   */
+  protected $categoryStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() : void {
+
+    parent::setUp();
+    $this->bookFactory = new BookFactory();
+    $this->bookStorage = Book::getStorage();
+    $this->categoryFactory = new CategoryFactory();
+    $this->categoryStorage = Category::getStorage();
+    $this->categoryStorage->dropSchema();
+    $this->bookStorage->dropSchema();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown(): void {
+
+    parent::tearDown();
+    $this->bookStorage->dropSchema();
+    $this->categoryStorage->dropSchema();
+  }
+
+}
--- web_service/tests/Unit/CategoryBookRelationTest.php
+++ web_service/tests/Unit/CategoryBookRelationTest.php
@@ -2,73 +2,15 @@
 
 namespace Tests\Unit;
 
-use RestfulWS\Core\Components\Factory\BookFactory;
-use RestfulWS\Core\Components\Factory\CategoryFactory;
-use RestfulWS\Core\Components\Factory\ModelFactoryInterface;
-use RestfulWS\Core\Components\Model\Book;
-use RestfulWS\Core\Components\Model\Category;
 use RestfulWS\Core\Components\Model\ModelInterface;
-use RestfulWS\Core\Components\Storage\StorageInterface;
-use Tests\KernelTestCase;
+use Tests\MultipleModelTestCase;
 
 /**
  * Class CategoryBookRelationTest.
  *
  * @package Tests\Unit
  */
-class CategoryBookRelationTest extends KernelTestCase {
-
-  /**
-   * Generator.
-   *
-   * @var ModelFactoryInterface
-   */
-  protected $bookFactory;
-
-  /**
-   * Storage.
-   *
-   * @var StorageInterface
-   */
-  protected $bookStorage;
-
-  /**
-   * Generator
-   *
-   * @var ModelFactoryInterface
-   */
-  protected $categoryFactory;
-
-  /**
-   * Storage.
-   *
-   * @var StorageInterface
-   */
-  protected $categoryStorage;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() : void {
-
-    parent::setUp();
-    $this->bookFactory = new BookFactory();
-    $this->bookStorage = Book::getStorage();
-    $this->categoryFactory = new CategoryFactory();
-    $this->categoryStorage = Category::getStorage();
-    $this->categoryStorage->dropSchema();
-    $this->bookStorage->dropSchema();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function tearDown(): void {
-
-    parent::tearDown();
-    $this->bookStorage->dropSchema();
-    $this->categoryStorage->dropSchema();
-  }
+class CategoryBookRelationTest extends MultipleModelTestCase {
 
   /**
    * Test intermediate table creation.
